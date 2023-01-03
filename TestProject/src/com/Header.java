01acd485194cac8a359e18ae5fa91b4276ee255e
pasinduG99 <it20223380@my.sliit.lk>
2023-01-03 15:18:40 +0530

    Tested the Login

--- TestProject/src/com/Header.java
+++ TestProject/src/com/Header.java
@@ -1,9 +1,35 @@
 package com;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Header {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.setProperty("webDriver.chrome.driver","D://selenium jars and drivers//drivers//chromedriver/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		//maximize the window
+		driver.manage().window().maximize();
+		
+		//navigate to url
+		driver.navigate().to("https://www.amazon.com/");
+		
+		//Test Login
+		driver.findElement(By.id("nav-link-accountList-nav-line-1")).click();
+		
+		driver.findElement(By.name("email")).sendKeys("pasinduclash74@gmail.com"); 
+		
+		driver.findElement(By.xpath("//input[@id='continue']")).click(); 
+		
+		driver.findElement(By.id("ap_password")).sendKeys("Casio10#");
+		
+		driver.findElement(By.xpath("//input[@id='signInSubmit']")).click(); 
+		
+		
 
 	}
 
