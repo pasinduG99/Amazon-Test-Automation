cba3cc483c0a7fac8c73df4851039a3ca0dfd84e
pasinduG99 <it20223380@my.sliit.lk>
2023-01-05 09:55:29 +0530

    Tested the sell products in footer

--- TestProject/src/com/Footer.java
+++ TestProject/src/com/Footer.java
@@ -1,5 +1,41 @@
 package com;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Footer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webDriver.chrome.driver","D://selenium jars and drivers//drivers//chromedriver/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		//maximize the window
+		driver.manage().window().maximize();
+		
+		driver.get("https://www.amazon.com/");
+		
+		driver.findElement(By.xpath("//a[normalize-space()='Sell products on Amazon']")).click();
+		
+		driver.getWindowHandle();
+		
+		//Store the ID of the original window
+		String originalWindow = driver.getWindowHandle();
+
+		//Check we don't have other windows open already
+		assert driver.getWindowHandles().size() == 1;
+
+		//Click the link which opens in a new window
+		driver.findElement(By.xpath("//img[@alt='Twitter logo']")).click();
+		
+		//Switch back to the old tab 
+		driver.switchTo().window(originalWindow);
+		
+		//close window
+		driver.close();
+
+	}
 
 }
